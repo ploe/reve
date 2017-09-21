b3b8472444cc99fdc15b77a1c50d5993dfc5df87
Myke Atkinson <ploe@hotmail.co.uk>
2017-09-21 21:03:32 +0100

    Added Lua dependency.

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 compile = cc -g -O2 -I./hashish -I./murp ./hashish/hashish.o ./murp/murp.o
 SDL_FLAGS := `pkg-config sdl2 SDL2_image --cflags --libs`
+LUA_FLAGS := `pkg-config lua-5.2 --cflags --libs`
 
 all:
-	$(compile) Crew.c Player.c Stage.c Utils.c main.c -o rv  $(SDL_FLAGS)
+	$(compile) Crew.c Player.c Stage.c Utils.c main.c -o rv  $(SDL_FLAGS) $(LUA_FLAGS)
--- main.c
+++ main.c
@@ -1,19 +1,32 @@
 #include "reve.h"
 
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
+
 typedef struct {
 	int *map;
 	SDL_Surface *surface;
 	rv_Bool enabled;
 } rv_Layer;
 
-int main() {
+int main(int argc, char *argv[]) {
+	lua_State *L = NULL;
+	L = luaL_newstate();
+	luaL_openlibs(L);
+
+	int i;
+	for (i = 1; i < argc; i++) {
+		if (luaL_loadfile(L, argv[i]) || lua_pcall(L, 0, 0, 0)) {
+			fprintf(stderr, "%s\n", lua_tostring(L, -1));
+			lua_pop(L, 1);
+		}
+	}
+	lua_close(L);
 
 	/* All the world's a stage,	*/
 	rv_CrewNew(rv_STAGE);
-//	char *str = rv_CrewMarshal();
-//	puts(str);
-//	mp_Atomize(str, rv_PrintAtom);
-//	free(str);
+
 	while (rv_CrewRoll()) continue;	
 	rv_CrewPurge();
 	/* Sans teeth, sans eyes, sans taste, sans everything. */
