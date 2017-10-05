889b7db00b82434c063bc4c7e0b7570682bb2083
Myke Atkinson <ploe@hotmail.co.uk>
2017-10-05 16:32:28 +0100

    SQLite3 added. This and Lua need encapsulating better. They will be part of rv_STAGE.

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ compile = cc -g -O2 -I./libs/hashish -I./libs/murp -I./include ./libs/hashish/ha
 src = src/Crew.c src/Player.c src/Texture.c src/Layer.c src/Stage.c src/Utils.c src/main.c
 SDL_FLAGS := `pkg-config sdl2 SDL2_image --cflags --libs`
 LUA_FLAGS := `pkg-config lua-5.2 --cflags --libs`
+SQLITE_FLAGS := `pkg-config sqlite3 --cflags --libs`
 
 all:
-	$(compile) $(src) -o ../rv  $(SDL_FLAGS) $(LUA_FLAGS)
+	$(compile) $(src) -o ./rv  $(SDL_FLAGS) $(LUA_FLAGS) $(SQLITE_FLAGS)
--- src/main.c
+++ src/main.c
@@ -4,11 +4,34 @@
 #include "lauxlib.h"
 #include "lualib.h"
 
+#include "sqlite3.h"
+
 typedef struct {
 	int *map;
 	SDL_Surface *surface;
 	rv_Bool enabled;
 } rv_Layer;
+
+void SQLLoad() {
+	sqlite3 *db;
+	char *zErrMsg = NULL;
+	int rc;
+
+	rc = sqlite3_open("slot1.db", &db);
+	if (rc) {
+		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+		sqlite3_close(db);
+		return;
+	}
+
+	rc = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS mmmmm(id PRIMARY KEY NOT NULL, key TEXT, value TEXT)", NULL, 0, &zErrMsg);
+	if (rc != SQLITE_OK) {
+		fprintf(stderr, "SQL error: %s\n", zErrMsg);
+		sqlite3_free(zErrMsg);
+	}
+
+	sqlite3_close(db);
+}
 
 int main(int argc, char *argv[]) {
 	lua_State *L = NULL;
@@ -23,6 +46,8 @@ int main(int argc, char *argv[]) {
 		}
 	}
 	lua_close(L);
+
+	SQLLoad();
 
 	/* All the world's a stage,	*/
 	rv_CrewNew(rv_STAGE);
