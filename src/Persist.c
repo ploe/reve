4c603f7f1e942b9a0cf388ac255c3cb4c4114909
Myke Atkinson <ploe@hotmail.co.uk>
2017-10-07 09:19:30 +0100

    [WIP] Saving.

--- src/Persist.c
+++ src/Persist.c
@@ -1,6 +1,5 @@
 #include "reve.h"
 
-
 rv_Bool rv_PersistCreateTable(sqlite3 *db, const char *table) {
 	char *schema = rv_TextNew("CREATE TABLE IF NOT EXISTS %s(id PRIMARY KEY NOT NULL, key TEXT, value TEXT)", table);
 	int rc;
@@ -14,5 +13,23 @@ rv_Bool rv_PersistCreateTable(sqlite3 *db, const char *table) {
 	}
 
 	rv_TextFree(schema);
+	return rv_YES;
+}
+
+rv_Bool rv_PersistPair(sqlite3 *db, const char *table, const char *key, const char *value) {
+	const char *query = "INSERT INTO %s (key, value) VALUES(\"hello\", \"world\")";
+	rv_Text = rv_TextNew(query, table);
+
+	int rc;
+	char *err = NULL;
+
+	rc = sqlite3_exec(db, query, NULL, 0, &err);
+	if (rc != SQLITE_OK) {
+		
+		rv_TextFree(query);
+		rv_Panic(-1, err);
+	}
+
+	rv_TextFree(query);
 	return rv_YES;
 }
